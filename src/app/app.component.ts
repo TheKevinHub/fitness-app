ef703a2f6af9fb9dbc65427911e13b558d88a731
TheKevinHub <kevin.accetta95@gmail.com>
2024-04-13 15:06:36 -0400

    DatePipe functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'fitness-app';
+
+  dateTime: Date
+
+  ngOnInit() {
+    timer(0, 1000).subscribe(() => {
+      this.dateTime = new Date()
+    })
+  }
 }
+
+
+// import { Observable, timer } from 'rxjs';
+// import { map } from 'rxjs/operators';
+
+// dateTime: Observable<Date>
+
+// ngOnInit() {
+//   this.dateTime = timer(0, 1000).pipe (
+//     map(() => {
+//       return new Date()
+//     })
+//   )
+// }
+// Add | async
