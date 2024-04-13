e70ceb89e0120c7e2eb3860acd629d4c17c629ab
TheKevinHub <kevin.accetta95@gmail.com>
2024-04-13 16:28:56 -0400

    Add new exercises and filtering functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { timer } from 'rxjs';
+import { ExerciseItem } from '../shared/models/exerciseItem';
+import { FormsModule } from '@angular/forms';
+
+const filters = [
+  (item : ExerciseItem) => item,
+  (item : ExerciseItem) => !item.isCustom,
+  (item : ExerciseItem) => item.isCustom,
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,6 +29,34 @@ export class AppComponent implements OnInit {
       this.dateTime = new Date()
     })
   }
+
+
+  // Testing adding new exercises and working with filters
+  // PS. Remember to remove bootstrap from index.html
+
+  items : ExerciseItem[] = [
+    new ExerciseItem('Push Up'),
+    new ExerciseItem('Pull Up'),
+    new ExerciseItem('Chin Up', true)
+  ];
+
+  listFilter : any = '0';
+
+  newExerciseName = '';
+
+  get visibleItems() : ExerciseItem[] {
+    return this.items.filter(filters[this.listFilter])
+  }
+
+  addNewExercise() {
+    this.items.push(new ExerciseItem(this.newExerciseName, true))
+    this.newExerciseName = '';
+  }
+
+  toggleItem(item : ExerciseItem) {
+    item.isCustom = !item.isCustom;
+  }
+
 }
 
 
---
+++ src/shared/models/exerciseItem.ts
@@ -0,0 +1,5 @@
+export class ExerciseItem {
+    constructor(public exerciseName : string, public isCustom : boolean = false) {
+
+    }
+}
