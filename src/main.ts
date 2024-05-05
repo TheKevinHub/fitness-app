1d8cc6f0a5a7eb31349b4a10cf3cf28d1c203a4a
TheKevinHub <kevin.accetta95@gmail.com>
2024-05-05 15:48:37 -0400

    Added backend fetch

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,9 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+// Fetch from backend
+fetch('localhost:8080/api/content')
+.then(response => response.json())
+.then(data => console.log(data));
